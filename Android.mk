27e66b721ed6472fb319b0421ca460713a1b2df5
Ajay Dudani <adudani@codeaurora.org>
2013-08-19 18:56:50 -0700

    keymaster: Enable compilation for msm8226 based platforms
    Enable keymaster for msm8226.
    
    Change-Id: Ibd8b0e935f01986c2920f75e04671413817ea980

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter msm8960 msm8974,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8974 msm8226,$(TARGET_BOARD_PLATFORM)),)
 
 include $(CLEAR_VARS)
 
