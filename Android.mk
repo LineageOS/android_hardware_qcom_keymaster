b174779262d622d82670d9e11a6baf645c08e068
Jyoti Wadhwani <jyotiw@codeaurora.org>
2014-10-17 11:57:10 -0700

    Keymaster: Support for 64bit userspace and 32bit TZ
    HW keymaster is not supported for 64bit userspace. This patch
    updates variable types to support the architecture.
    It also makes change in Android.mk file to support all qcom
    platforms.
    
    Change-Id: I25633d707b425388bcc116196c0e0022c5c4f9a5

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter msm8960 msm8974 msm8226 msm8084,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(call is-vendor-board-platform,QCOM),true)
 
 keymaster-def := -fvisibility=hidden -Wall
 ifeq ($(TARGET_BOARD_PLATFORM),msm8084)
@@ -9,13 +9,14 @@ endif
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := keystore.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := keystore.qcom
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 
 LOCAL_SRC_FILES := keymaster_qcom.cpp
 
-LOCAL_C_INCLUDES := $(TARGET_OUT_HEADERS)/common/inc
+LOCAL_C_INCLUDES := $(TARGET_OUT_HEADERS)/common/inc \
+                    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 LOCAL_CFLAGS := $(keymaster-def)
 
