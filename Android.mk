30c28741c30c1ed76bc76a9b16c91bc59b050ce3
Steve Kondik <steve@cyngn.com>
2014-11-10 09:35:06 -0800

    keymaster: Add kernel dependency
    Change-Id: I6ff0b7995ac19d272c088e6210638cdcd48bc4de
    
    keymaster: Use kernel headers
    
    Change-Id: Ic0ffc21830d9cae4415747b79d42219c3ca849dc

--- Android.mk
+++ Android.mk
@@ -17,7 +17,9 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 LOCAL_SRC_FILES := keymaster_qcom.cpp
 
-LOCAL_C_INCLUDES := $(TARGET_OUT_HEADERS)/common/inc
+LOCAL_C_INCLUDES := \
+    $(TARGET_OUT_HEADERS)/common/inc \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 
 LOCAL_CFLAGS := $(keymaster-def)
 
@@ -27,7 +29,9 @@ LOCAL_SHARED_LIBRARIES := \
         libc \
         libdl
 
-LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr \
+    $(LOCAL_PATH)/Android.mk
 
 LOCAL_MODULE_TAGS := optional
 
