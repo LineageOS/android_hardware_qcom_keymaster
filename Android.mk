e9057865ed3042cfd7f49f9e4118d57d63d3998f
Ethan Chen <intervigil@gmail.com>
2015-01-09 11:54:48 -0800

    keymaster: Set HEAP_MASK_COMPATIBILITY by platform for QCOM_HARDWARE
    * Google kernels have a separate hack to handle this, only set by
      platform for QCOM_HARDWARE devices.
    
    Change-Id: Iab91cee4922e1b3467691a04eb1c5061705fbab3

--- Android.mk
+++ Android.mk
@@ -3,8 +3,14 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8916 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084,$(TARGET_BOARD_PLATFORM)),)
+keymaster-def += -D_ION_HEAP_MASK_COMPATIBILITY_WA
+endif
+else
 ifeq ($(TARGET_BOARD_PLATFORM),msm8084)
 keymaster-def += -D_ION_HEAP_MASK_COMPATIBILITY_WA
+endif
 endif
 
 include $(CLEAR_VARS)
