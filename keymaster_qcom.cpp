6bb78f3972e42eaf4c5a0770f28673b7ce547780
myfluxi <linflux@arcor.de>
2015-11-04 00:46:09 +0100

    keymaster: Featureize support for waiting on QSEE to start
    New devices need to wait for QSEE to load the Keymaster into TZ.
    This is signalled by a property being set by qseecomd. Unfortunately
    we don't have a good way to determine if this is the case for
    a particular device, so allow opting out of it.
    
    Set TARGET_KEYMASTER_SKIP_WAITING_FOR_QSEE=true to revert
    the new behavior.
    
    Change-Id: I99839b82fdd8ab6550a95e2676a5da03eccfe288

--- keymaster_qcom.cpp
+++ keymaster_qcom.cpp
@@ -743,7 +743,9 @@ static int qcom_km_open(const hw_module_t* module, const char* name,
 {
     int ret = 0;
     unsigned int attempt_num = 0;
+#ifndef SKIP_WAITING_FOR_QSEE
     char property_val[PROPERTY_VALUE_MAX] = {0};
+#endif
     qcom_keymaster_handle_t* km_handle;
     if (strcmp(name, KEYSTORE_KEYMASTER) != 0)
         return -EINVAL;
@@ -768,12 +770,16 @@ static int qcom_km_open(const hw_module_t* module, const char* name,
     dev->context = (void *)km_handle;
     while (attempt_num < MAX_PROPERTY_GET_ATTEMPTS)
     {
+#ifndef SKIP_WAITING_FOR_QSEE
         property_get("sys.keymaster.loaded", property_val, "");
         if (strncmp(property_val, "true", sizeof(property_val)) == 0)
         {
+#endif
             ALOGD("keymaster app is loaded");
             break;
+#ifndef SKIP_WAITING_FOR_QSEE
         }
+#endif
         if (attempt_num == 0)
             ALOGE("keymaster app is not loaded, attempt number %d", attempt_num);
         attempt_num++;
