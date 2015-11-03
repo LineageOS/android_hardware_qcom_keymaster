c12b0cc7fd9ab0521fdf0f355bd28c6f2f7c18a3
myfluxi <linflux@arcor.de>
2015-11-04 00:46:09 +0100

    keymaster: Featureize support for waiting on QSEE to start
    New devices need to wait for QSEE to load the Keymaster into TZ.
    This is signalled by a property being set by qseecomd. Unfortunately
    we don't have a good way to determine if this is the case for
    a particular device, so make it opt-in.
    
    Set TARGET_KEYMASTER_WAIT_FOR_QSEE=true for this behavior.
    
    Change-Id: I99839b82fdd8ab6550a95e2676a5da03eccfe288

--- Android.mk
+++ Android.mk
@@ -12,6 +12,9 @@ ifeq ($(TARGET_BOARD_PLATFORM),msm8084)
 keymaster-def += -D_ION_HEAP_MASK_COMPATIBILITY_WA
 endif
 endif
+ifeq ($(TARGET_KEYMASTER_WAIT_FOR_QSEE),true)
+LOCAL_CFLAGS += -DWAIT_FOR_QSEE
+endif
 
 include $(CLEAR_VARS)
 
