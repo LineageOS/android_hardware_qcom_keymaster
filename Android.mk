68fe4a7352c4d22a106f6729e7981ed447e8056b
Adam Farden <adam@farden.cz>
2015-12-10 21:03:31 +0100

    Disable ION_HEAP_MASK_COMPATIBILITY_WA on Sony 3.10 Kernel
    Set SONY_BF64_KERNEL_VARIANT := true,
    the same flag as used in build/ to pick the HALs
    
    Signed-off-by: Adam Farden <adam@farden.cz>
    Signed-off-by: Chirayu Desai <chirayudesai1@gmail.com>
    Change-Id: Id08346823701bc7cd97414ca2743f3719a998973

--- Android.mk
+++ Android.mk
@@ -5,8 +5,10 @@ ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8916 msm8952 
 
 keymaster-def := -fvisibility=hidden -Wall
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+ifneq ($(SONY_BF64_KERNEL_VARIANT),true)
 ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084,$(TARGET_BOARD_PLATFORM)),)
 keymaster-def += -D_ION_HEAP_MASK_COMPATIBILITY_WA
+endif
 endif
 else
 ifeq ($(TARGET_BOARD_PLATFORM),msm8084)
