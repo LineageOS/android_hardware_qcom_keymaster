cd302b350d865d8076eeff18d770b29c0fc4c47e
Edward Wang <ewang@cyngn.com>
2016-02-23 17:35:37 -0800

    keymaster: add msm8909 into support list
    Issue-Id: FEIJ-8
    
    Change-Id: I1252ec5f1171ca94635e6f58d835a588948be771

--- Android.mk
+++ Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
-ifneq ($(filter msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8909 msm8960 msm8226 msm8974 msm8610 msm8084 apq8084 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
