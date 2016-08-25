8a407deda9c5f9808db3193a147aa4147b5a247e
Chirayu Desai <chirayudesai1@gmail.com>
2016-08-25 20:45:19 +0530

    Revert "Remove legacy keymaster support for msm8226"
    This reverts commit 888834f9aba0609222c6e6bbd86bd6625af28746.
    
    Change-Id: I2a6cceb2adb7c217976f6c31bb0943dc5d064ae8

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter msm8960 msm8974 msm8084,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8974 msm8226 msm8084,$(TARGET_BOARD_PLATFORM)),)
 
 keymaster-def := -fvisibility=hidden -Wall
 ifeq ($(TARGET_BOARD_PLATFORM),msm8084)
